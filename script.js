dcc0b9178c862d610857354e9e615964a7fda7f0
JB Sinluenam <jb.sinluenam@gmail.com>
2022-09-30 12:10:50 -0700

    add reset button and function

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ let scoreHome = 0;
 let scoreGuest = 0;
 let scoreHomeEl = document.getElementById("scoreHome");
 let scoreGuestEL = document.getElementById("scoreGuest");
-scoreHomeEl.innerText = 0;
-scoreGuestEL.innerText = 0;
 
 function addOne() {
   scoreHome += 1;
@@ -23,4 +21,28 @@ function addTwo() {
 function addThree() {
   scoreHome += 3;
   scoreHomeEl.innerText = scoreHome;
+}
+
+function addOneForGuest() {
+  scoreGuest += 1;
+  scoreGuestEL.innerText = scoreGuest;
+}
+
+function addTwoForGuest() {
+  scoreGuest += 2;
+  scoreGuestEL.innerText = scoreGuest;
+}
+
+function addThreeForGuest() {
+  scoreGuest += 3;
+  scoreGuestEL.innerText = scoreGuest;
+}
+
+let resetEl = document.getElementById("reset");
+scoreHomeEl.innerText = 0;
+scoreGuestEL.innerText = 0;
+
+function reset() {
+  scoreHomeEl.innerText = 0;
+  scoreGuestEL.innerText = 0;
 }
